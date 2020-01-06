f5105a6ce0054b2f4b608d1453d6804ba02a327a
Aitao Hu <aitaohu@nucleisys.com>
2020-01-06 15:15:41 +0800

    SoC:add GD32vf103 SOC code
    Signed-off-by: Aitao Hu <aitaohu@nucleisys.com>

---
+++ SoC/Gd32vf103/Common/Include/soc.h
@@ -0,0 +1,17 @@
+// See LICENSE for license details.
+
+#ifndef __SOC_H__
+#define __SOC_H__
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+/****************************************************************************
+ * Platform definitions
+ *****************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SOC_H__ */
---
+++ SoC/Gd32vf103/Common/Source/Stubs/write.c
@@ -0,0 +1,25 @@
+/* See LICENSE of license details. */
+
+#include <stdint.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <gd32vf103_hal.h>
+#include "gd32vf103_usart.h"
+
+
+ssize_t _write(int fd, const void* ptr, size_t len)
+{
+    if (!isatty(fd)) {
+        return -1;
+    }
+
+    const uint8_t * writebuf = (const uint8_t *)ptr;
+    for (size_t i = 0; i < len; i ++) {
+        if (writebuf[i] == '\n') {
+            usart_write(USART0,'\r');
+        }
+        usart_write(USART0,writebuf[i]);
+    }
+    return len;
+}
