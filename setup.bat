692d76a156deb6109c34dc5aedd3c0e8ac203795
Huaqi Fang <578567190@qq.com>
2020-02-19 15:28:25 +0800

    env: Add setup message in setup environment script
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- setup.bat
+++ setup.bat
@@ -10,6 +10,9 @@ set SETUP_CONFIG=setup_config.bat
 
 if exist %SETUP_CONFIG% call %SETUP_CONFIG%
 
+echo Setup Nuclei SDK Tool Environment
+echo NUCLEI_TOOL_ROOT=%NUCLEI_TOOL_ROOT%
+
 set PATH=%NUCLEI_TOOL_ROOT%\gcc\bin;%NUCLEI_TOOL_ROOT%\openocd\bin;%NUCLEI_TOOL_ROOT%\build-tools\bin;%PATH%
 set NMSIS_ROOT=%NMSIS_ROOT%
 @echo on
--- setup.sh
+++ setup.sh
@@ -11,5 +11,8 @@ SETUP_CONFIG=setup_config.sh
 [ -f .ci/build_sdk.sh ] && source .ci/build_sdk.sh
 [ -f .ci/build_applications.sh ] && source .ci/build_applications.sh
 
+echo "Setup Nuclei SDK Tool Environment"
+echo "NUCLEI_TOOL_ROOT=$NUCLEI_TOOL_ROOT"
+
 export PATH=$NUCLEI_TOOL_ROOT/gcc/bin:$NUCLEI_TOOL_ROOT/openocd/bin:$PATH
 export NMSIS_ROOT=$(readlink -f $NMSIS_ROOT)
