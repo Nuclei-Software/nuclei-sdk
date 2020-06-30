6768ee20a76eaa803121bacd6c730dc8302ac5dd
xi_ci <xi_ci@nucleisys.com>
2020-06-30 04:07:51 -0700

    env: Remove NMSIS_ROOT definition in setup.*
    Signed-off-by: xi_ci <xi_ci@nucleisys.com>

--- setup.bat
+++ setup.bat
@@ -1,11 +1,9 @@
 @echo off
 set NUCLEI_TOOL_ROOT=C:\Nuclei
-set NMSIS_ROOT=..\NMSIS
 
 REM Create your setup_config.bat
-REM and define NUCLEI_TOOL_ROOT or NMSIS_ROOT like below
+REM and define NUCLEI_TOOL_ROOT like below
 REM set NUCLEI_TOOL_ROOT=D:\Software\Nuclei
-REM set NMSIS_ROOT=D:\Source\NMSIS
 set SETUP_CONFIG=setup_config.bat
 
 if exist %SETUP_CONFIG% call %SETUP_CONFIG%
@@ -14,5 +12,4 @@ echo Setup Nuclei SDK Tool Environment
 echo NUCLEI_TOOL_ROOT=%NUCLEI_TOOL_ROOT%
 
 set PATH=%NUCLEI_TOOL_ROOT%\gcc\bin;%NUCLEI_TOOL_ROOT%\openocd\bin;%NUCLEI_TOOL_ROOT%\build-tools\bin;%PATH%
-set NMSIS_ROOT=%NMSIS_ROOT%
 @echo on
--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 NUCLEI_TOOL_ROOT=~/Nuclei
-NMSIS_ROOT=../NMSIS
 # Create your setup_config.sh
-# and define NUCLEI_TOOL_ROOT and NMSIS_ROOT like below
+# and define NUCLEI_TOOL_ROOT like below
 # NUCLEI_TOOL_ROOT=/home/develop/Software/Nuclei
-# NMSIS_ROOT=/home/develop/Source/NMSIS
 SETUP_CONFIG=setup_config.sh
 
 [ -f $SETUP_CONFIG ] && source $SETUP_CONFIG
@@ -15,4 +13,3 @@ echo "Setup Nuclei SDK Tool Environment"
 echo "NUCLEI_TOOL_ROOT=$NUCLEI_TOOL_ROOT"
 
 export PATH=$NUCLEI_TOOL_ROOT/gcc/bin:$NUCLEI_TOOL_ROOT/openocd/bin:$PATH
-export NMSIS_ROOT=$(readlink -f $NMSIS_ROOT)
