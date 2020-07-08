af8ae6ff994a357dd98a7575c0520322d4712707
Huaqi Fang <578567190@qq.com>
2020-07-08 15:41:26 +0800

    SoC: Format hbird.h header file
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/hbird/Common/Include/hbird.h
+++ SoC/hbird/Common/Include/hbird.h
@@ -200,9 +200,9 @@ typedef enum EXCn {
   #warning Not supported compiler type
 #endif
 
-#define RTC_FREQ 32768
+#define RTC_FREQ                    32768
 // The TIMER frequency is just the RTC frequency
-#define SOC_TIMER_FREQ RTC_FREQ
+#define SOC_TIMER_FREQ              RTC_FREQ
 /* =========================================================================================================================== */
 /* ================                            Device Specific Peripheral Section                             ================ */
 /* =========================================================================================================================== */
@@ -216,46 +216,46 @@ typedef enum EXCn {
  * Platform definitions
  *****************************************************************************/
 // IOF Mappings
-#define IOF0_SPI1_MASK          _AC(0x000007FC,UL)
-#define SPI11_NUM_SS            (4)
-#define IOF_SPI1_SS0            (2u)
-#define IOF_SPI1_SS1            (8u)
-#define IOF_SPI1_SS2            (9u)
-#define IOF_SPI1_SS3            (10u)
-#define IOF_SPI1_MOSI           (3u)
-#define IOF_SPI1_MISO           (4u)
-#define IOF_SPI1_SCK            (5u)
-#define IOF_SPI1_DQ0            (3u)
-#define IOF_SPI1_DQ1            (4u)
-#define IOF_SPI1_DQ2            (6u)
-#define IOF_SPI1_DQ3            (7u)
+#define IOF0_SPI1_MASK              _AC(0x000007FC,UL)
+#define SPI11_NUM_SS                (4)
+#define IOF_SPI1_SS0                (2u)
+#define IOF_SPI1_SS1                (8u)
+#define IOF_SPI1_SS2                (9u)
+#define IOF_SPI1_SS3                (10u)
+#define IOF_SPI1_MOSI               (3u)
+#define IOF_SPI1_MISO               (4u)
+#define IOF_SPI1_SCK                (5u)
+#define IOF_SPI1_DQ0                (3u)
+#define IOF_SPI1_DQ1                (4u)
+#define IOF_SPI1_DQ2                (6u)
+#define IOF_SPI1_DQ3                (7u)
 
-#define IOF0_SPI2_MASK          _AC(0xFC000000,UL)
-#define SPI2_NUM_SS             (1)
-#define IOF_SPI2_SS0            (26u)
-#define IOF_SPI2_MOSI           (27u)
-#define IOF_SPI2_MISO           (28u)
-#define IOF_SPI2_SCK            (29u)
-#define IOF_SPI2_DQ0            (27u)
-#define IOF_SPI2_DQ1            (28u)
-#define IOF_SPI2_DQ2            (30u)
-#define IOF_SPI2_DQ3            (31u)
+#define IOF0_SPI2_MASK              _AC(0xFC000000,UL)
+#define SPI2_NUM_SS                 (1)
+#define IOF_SPI2_SS0                (26u)
+#define IOF_SPI2_MOSI               (27u)
+#define IOF_SPI2_MISO               (28u)
+#define IOF_SPI2_SCK                (29u)
+#define IOF_SPI2_DQ0                (27u)
+#define IOF_SPI2_DQ1                (28u)
+#define IOF_SPI2_DQ2                (30u)
+#define IOF_SPI2_DQ3                (31u)
 
-#define IOF0_UART0_MASK         _AC(0x00030000, UL)
-#define IOF_UART0_RX            (16u)
-#define IOF_UART0_TX            (17u)
+#define IOF0_UART0_MASK             _AC(0x00030000, UL)
+#define IOF_UART0_RX                (16u)
+#define IOF_UART0_TX                (17u)
 
-#define IOF0_UART1_MASK         _AC(0x03000000, UL)
-#define IOF_UART1_RX            (24u)
-#define IOF_UART1_TX            (25u)
+#define IOF0_UART1_MASK             _AC(0x03000000, UL)
+#define IOF_UART1_RX                (24u)
+#define IOF_UART1_TX                (25u)
 
-#define IOF0_I2C_MASK           _AC(0x00003000, UL)
-#define IOF_I2C_SDA             (12u)
-#define IOF_I2C_SCL             (13u)
+#define IOF0_I2C_MASK               _AC(0x00003000, UL)
+#define IOF_I2C_SDA                 (12u)
+#define IOF_I2C_SCL                 (13u)
 
-#define IOF1_PWM0_MASK          _AC(0x0000000F, UL)
-#define IOF1_PWM1_MASK          _AC(0x00780000, UL)
-#define IOF1_PWM2_MASK          _AC(0x00003C00, UL)
+#define IOF1_PWM0_MASK              _AC(0x0000000F, UL)
+#define IOF1_PWM1_MASK              _AC(0x00780000, UL)
+#define IOF1_PWM2_MASK              _AC(0x00003C00, UL)
 
 // Interrupt Numbers
 #define SOC_ECLIC_NUM_INTERRUPTS    32
@@ -292,7 +292,7 @@ typedef struct {  /*!< GPIO Structure */
 } GPIO_TypeDef;
 
 /**
-  * @brief UART (UART)
+  * @brief UART
   */
 typedef struct {
     __IOM uint32_t TXFIFO;
@@ -304,6 +304,9 @@ typedef struct {
     __IOM uint32_t DIV;
 } UART_TypeDef;
 
+/**
+  * @brief PWM
+  */
 typedef struct {
     __IOM uint32_t CFG;
           uint32_t RESERVED0;
@@ -317,6 +320,9 @@ typedef struct {
     __IOM uint32_t CMP3;
 } PWM_TypeDef;
 
+/**
+  * @brief QSPI
+  */
 typedef struct {
     __IOM uint32_t SCKDIV;
     __IOM uint32_t SCKMODE;
@@ -342,6 +348,9 @@ typedef struct {
     __IOM uint32_t IP;
 } QSPI_TypeDef;
 
+/**
+  * @brief I2C
+  */
 typedef struct {
     __IOM uint8_t PRERlo;
     __IOM uint8_t PRERhi;
@@ -372,25 +381,25 @@ typedef struct {
   * @{
   */
 /* Peripheral and SRAM base address */
-#define QSPI_FLASH_BASE                       (0x20000000UL)      /*!< (FLASH     ) Base Address */
-#define ONCHIP_ROM_BASE                       (0x00001000UL)      /*!< (ROM       ) Base Address */
-#define ONCHIP_ILM_BASE                       (0x80000000UL)      /*!< (ILM       ) Base Address */
-#define ONCHIP_DLM_BASE                       (0x90000000UL)      /*!< (DLM       ) Base Address */
-#define HBIRD_PERIPH_BASE                (0x10000000UL)      /*!< (Peripheral) Base Address */
+#define QSPI_FLASH_BASE         (0x20000000UL)      /*!< (FLASH     ) Base Address */
+#define ONCHIP_ROM_BASE         (0x00001000UL)      /*!< (ROM       ) Base Address */
+#define ONCHIP_ILM_BASE         (0x80000000UL)      /*!< (ILM       ) Base Address */
+#define ONCHIP_DLM_BASE         (0x90000000UL)      /*!< (DLM       ) Base Address */
+#define HBIRD_PERIPH_BASE       (0x10000000UL)      /*!< (Peripheral) Base Address */
 
 /* Peripheral memory map */
 /* Fast-IO Interfaced IP */
-#define GPIO_BASE         (HBIRD_PERIPH_BASE + 0x12000)          /*!< (GPIO) Base Address */
+#define GPIO_BASE               (HBIRD_PERIPH_BASE + 0x12000)          /*!< (GPIO) Base Address */
 /* PPI Interfaced IP */
-#define UART0_BASE        (HBIRD_PERIPH_BASE + 0x13000)          /*!< (UART0) Base Address */
-#define QSPI0_BASE        (HBIRD_PERIPH_BASE + 0x14000)          /*!< (QSPI0) Base Address */
-#define PWM0_BASE         (HBIRD_PERIPH_BASE + 0x15000)          /*!< (PWM0) Base Address */
-#define UART1_BASE        (HBIRD_PERIPH_BASE + 0x23000)          /*!< (UART1) Base Address */
-#define QSPI1_BASE        (HBIRD_PERIPH_BASE + 0x24000)          /*!< (QSPI1) Base Address */
-#define PWM1_BASE         (HBIRD_PERIPH_BASE + 0x25000)          /*!< (PWM1) Base Address */
-#define QSPI2_BASE        (HBIRD_PERIPH_BASE + 0x34000)          /*!< (QSPI2) Base Address */
-#define PWM2_BASE         (HBIRD_PERIPH_BASE + 0x35000)          /*!< (PWM2) Base Address */
-#define I2C_BASE          (HBIRD_PERIPH_BASE + 0x42000)          /*!< (I2C Master) Base Address */
+#define UART0_BASE              (HBIRD_PERIPH_BASE + 0x13000)          /*!< (UART0) Base Address */
+#define QSPI0_BASE              (HBIRD_PERIPH_BASE + 0x14000)          /*!< (QSPI0) Base Address */
+#define PWM0_BASE               (HBIRD_PERIPH_BASE + 0x15000)          /*!< (PWM0) Base Address */
+#define UART1_BASE              (HBIRD_PERIPH_BASE + 0x23000)          /*!< (UART1) Base Address */
+#define QSPI1_BASE              (HBIRD_PERIPH_BASE + 0x24000)          /*!< (QSPI1) Base Address */
+#define PWM1_BASE               (HBIRD_PERIPH_BASE + 0x25000)          /*!< (PWM1) Base Address */
+#define QSPI2_BASE              (HBIRD_PERIPH_BASE + 0x34000)          /*!< (QSPI2) Base Address */
+#define PWM2_BASE               (HBIRD_PERIPH_BASE + 0x35000)          /*!< (PWM2) Base Address */
+#define I2C_BASE                (HBIRD_PERIPH_BASE + 0x42000)          /*!< (I2C Master) Base Address */
 
 /** @} */ /* End of group Device_Peripheral_peripheralAddr */
 
@@ -405,16 +414,16 @@ typedef struct {
 /** @addtogroup Device_Peripheral_declaration
   * @{
   */
-#define GPIO        ((GPIO_TypeDef *) GPIO_BASE)
-#define UART0       ((UART_TypeDef *) UART0_BASE)
-#define QSPI0       ((QSPI_TypeDef *) QSPI0_BASE)
-#define PWM0        ((PWM_TypeDef *) PWM0_BASE)
-#define UART1       ((UART_TypeDef *) UART1_BASE)
-#define QSPI1       ((QSPI_TypeDef *) QSPI1_BASE)
-#define PWM1        ((PWM_TypeDef *) PWM1_BASE)
-#define QSPI2       ((QSPI_TypeDef *) QSPI2_BASE)
-#define PWM2        ((PWM_TypeDef *) PWM2_BASE)
-#define I2C         ((I2C_TypeDef *) I2C_BASE)
+#define GPIO                    ((GPIO_TypeDef *) GPIO_BASE)
+#define UART0                   ((UART_TypeDef *) UART0_BASE)
+#define QSPI0                   ((QSPI_TypeDef *) QSPI0_BASE)
+#define PWM0                    ((PWM_TypeDef *) PWM0_BASE)
+#define UART1                   ((UART_TypeDef *) UART1_BASE)
+#define QSPI1                   ((QSPI_TypeDef *) QSPI1_BASE)
+#define PWM1                    ((PWM_TypeDef *) PWM1_BASE)
+#define QSPI2                   ((QSPI_TypeDef *) QSPI2_BASE)
+#define PWM2                    ((PWM_TypeDef *) PWM2_BASE)
+#define I2C                     ((I2C_TypeDef *) I2C_BASE)
 
 // Helper functions
 #define _REG8(p, i)             (*(volatile uint8_t *) ((p) + (i)))
