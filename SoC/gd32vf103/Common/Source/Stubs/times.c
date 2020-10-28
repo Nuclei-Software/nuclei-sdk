5292d746b79f492c81e740b93d7e3bd973885cfc
Mika Tuupola <tuupola@appelsiini.net>
2020-10-28 12:09:42 +0200

    Fix return value
    Upon successful completion, times() shall return the elapsed real time,
    in clock ticks, since an arbitrary point in the past.

--- SoC/gd32vf103/Common/Source/Stubs/times.c
+++ SoC/gd32vf103/Common/Source/Stubs/times.c
@@ -4,12 +4,12 @@
 #include <sys/times.h>
 #include "nuclei_sdk_soc.h"
 
-int _times(struct tms *buf)
+clock_t _times(struct tms *buf)
 {
     buf->tms_cstime = 0;
     buf->tms_cutime = 0;
     buf->tms_stime = __get_rv_cycle();
     buf->tms_utime = 0;
 
-    return  0;
+    return buf->tms_utime;
 }
