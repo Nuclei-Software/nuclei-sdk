d782d834b419c734f84d34c44ef84fb7a2edeadf
Roman Buchert <roman.buchert@googlemail.com>
2020-11-25 14:19:57 +0100

    Template for sipeed longan nano

---
+++ SoC/gd32vf103/Board/gd32vf103v_longan_nano/Include/nuclei_sdk_hal.h
@@ -0,0 +1,20 @@
+// See LICENSE for license details.
+#ifndef _NUCLEI_SDK_HAL_H
+#define _NUCLEI_SDK_HAL_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "gd32vf103v_longan_nano.h"
+
+#define SOC_DEBUG_UART      GD32_COM0
+
+#ifndef NUCLEI_BANNER
+#define NUCLEI_BANNER       1
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
