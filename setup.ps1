329cdc0b95791dbdc664fa0481ba7575fec440cd
Huaqi Fang <578567190@qq.com>
2021-03-03 17:11:07 +0800

    env: update comments in setup.ps1
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,11 @@
 $NUCLEI_TOOL_ROOT="C:\Nuclei"
 
-# Create your setup_config.ps1
+# Create your setup_config.ps1 before run .\setup.ps1
 # and define $NUCLEI_TOOL_ROOT like below
-# set $NUCLEI_TOOL_ROOT="D:\Software\Nuclei"
+# $NUCLEI_TOOL_ROOT="D:\Software\Nuclei"
+# Start Windows PowerShell with the "Run as Administrator" option
+# Run the following command below to enable running unsigned scripts:
+# set-executionpolicy remotesigned
 
 $SETUP_CONFIG="setup_config.ps1"
 
