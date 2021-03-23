2dee2bd08a3a40aa6da11c94fa97a755d915c362
Huaqi Fang <578567190@qq.com>
2021-03-23 11:20:29 +0800

    SoC: Add Nuclei demosoc support by rename hbird to demosoc
    - Nuclei demosoc is the evaluation SoC running on Nuclei FPGA
    evaluation board, which can be configured with different
    Nuclei RISC-V Processor
    - hbird SoC is renamed to demosoc
    - hbird Board is renamed to nuclei_fpga_eval
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ SoC/demosoc/Common/Include/nuclei_sdk_soc.h
@@ -0,0 +1,19 @@
+// See LICENSE for license details.
+#ifndef _NUCLEI_SDK_SOC_H
+#define _NUCLEI_SDK_SOC_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "demosoc.h"
+#include "demosoc_uart.h"
+#include "demosoc_gpio.h"
+#include "demosoc_i2c.h"
+#include "demosoc_spi.h"
+#include "demosoc_pwm.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif
