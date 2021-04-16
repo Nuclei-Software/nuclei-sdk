d779117ad6987f974663ff1f87c69093ec6394e9
Huaqi Fang <578567190@qq.com>
2021-04-16 17:13:01 +0800

    application: Align freertos version in freertos app
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- application/freertos/demo/FreeRTOSConfig.h
+++ application/freertos/demo/FreeRTOSConfig.h
@@ -1,5 +1,6 @@
 /*
-    FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
+    FreeRTOS Kernel V10.3.1
+
     All rights reserved
 
     VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
