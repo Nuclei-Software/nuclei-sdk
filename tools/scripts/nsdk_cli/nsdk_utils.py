09342923be80eba8bf21b02ff4d638ff0c2e1ac3
Huaqi Fang <578567190@qq.com>
2021-04-22 10:32:43 +0800

    tools: add dict merge to replace dict update
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- tools/scripts/nsdk_cli/nsdk_utils.py
+++ tools/scripts/nsdk_cli/nsdk_utils.py
@@ -3,8 +3,10 @@
 import os
 import sys
 import time
+import copy
 import serial
 import tempfile
+import collections
 from threading import Thread
 import subprocess
 import asyncio
@@ -50,6 +52,23 @@ def save_json(file, data):
         print("Error: Data can't be serialized to json file!")
         return False
 
+# get from https://gist.github.com/angstwad/bf22d1822c38a92ec0a9
+def dict_merge(dct, merge_dct):
+    """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+    updating only top-level keys, dict_merge recurses down into dicts nested
+    to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+    ``dct``.
+    :param dct: dict onto which the merge is executed
+    :param merge_dct: dct merged into dct
+    :return: None
+    """
+    for k, v in merge_dct.items():
+        if (k in dct and isinstance(dct[k], dict)
+                and isinstance(merge_dct[k], collections.Mapping)):
+            dict_merge(dct[k], merge_dct[k])
+        else:
+            dct[k] = merge_dct[k]
+
 def get_make_csv(app, config):
     make_options = " "
     SUPPORT_KEYS = ["SOC", "BOARD", "CORE", "DOWNLOAD", "VARIANT", \
@@ -86,7 +105,7 @@ COMMAND_TIMEOUT=6
 RUNSTATUS_OK=0
 RUNSTATUS_FAIL=1
 RUNSTATUS_NOTSTART=2
-def run_command(command, show_output=True, logfile=None):
+def run_command(command, show_output=True, logfile=None, append=False):
     logfh = None
     ret = COMMAND_RUNOK
     cmd_elapsed_ticks = 0
@@ -96,7 +115,10 @@ def run_command(command, show_output=True, logfile=None):
     process = None
     try:
         if isinstance(logfile, str):
-            logfh = open(logfile, "wb")
+            if append:
+                logfh = open(logfile, "ab")
+            else:
+                logfh = open(logfile, "wb")
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, \
                             stderr=subprocess.STDOUT)
         while True:
