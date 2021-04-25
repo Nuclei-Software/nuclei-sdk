371b689dcfe292108e0ee4ee31a410510b896307
Huaqi Fang <578567190@qq.com>
2021-04-25 15:33:36 +0800

    env: add qemu into tool path
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- setup.ps1
+++ setup.ps1
@@ -16,4 +16,4 @@ if (Test-Path $SETUP_CONFIG) {
 Write-Output "Setup Nuclei SDK Tool Environment"
 Write-Output "NUCLEI_TOOL_ROOT=$NUCLEI_TOOL_ROOT"
 
-$env:Path="$NUCLEI_TOOL_ROOT\gcc\bin;$NUCLEI_TOOL_ROOT\openocd\bin;$NUCLEI_TOOL_ROOT\build-tools\bin;"+$env:Path
+$env:Path="$NUCLEI_TOOL_ROOT\gcc\bin;$NUCLEI_TOOL_ROOT\openocd\bin;$NUCLEI_TOOL_ROOT\build-tools\bin;$NUCLEI_TOOL_ROOT\qemu;"+$env:Path
