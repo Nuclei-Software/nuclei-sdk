05b3310292f7d556ee3d8a35137b2e6170860441
Huaqi Fang <578567190@qq.com>
2021-11-11 11:06:54 +0800

    build: add build.mk in NMSIS to provide NMSIS ability
    User can select NMSIS library using NMSIS_LIB now in application Makefile
    
    No need to manually add include directory and library selection now, just use
    NMSIS_LIB makefile variable now.
    
    eg.
    
    NMSIS_LIB ?= nmsis_dsp
    
    NMSIS_LIB ?= nmsis_dsp nmsis_nn
    
    NMSIS_LIB ?= nmsis_dsp
    ARCH_EXT ?= pv
    
    CORE ?= n307fd
    NMSIS_LIB_ARCH ?= rv32imac
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ NMSIS/build.mk
@@ -0,0 +1,25 @@
+NMSIS_LIB_SORTED := $(sort $(NMSIS_LIB))
+NMSIS_LIB_ARCH ?= $(RISCV_ARCH)
+
+INCDIRS += $(NUCLEI_SDK_NMSIS)/Core/Include
+
+ifeq ($(filter nmsis_dsp,$(NMSIS_LIB_SORTED)),nmsis_dsp)
+INCDIRS += $(NUCLEI_SDK_NMSIS)/DSP/Include \
+	$(NUCLEI_SDK_NMSIS)/DSP/PrivateInclude
+LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/DSP/GCC
+LDLIBS += -lnmsis_dsp_$(NMSIS_LIB_ARCH)
+endif
+
+ifeq ($(filter nmsis_nn,$(NMSIS_LIB_SORTED)),nmsis_nn)
+INCDIRS += $(NUCLEI_SDK_NMSIS)/NN/Include
+LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/NN/GCC
+LDLIBS += -lnmsis_nn_$(NMSIS_LIB_ARCH)
+endif
+
+ifneq ($(findstring p,$(ARCH_EXT)),)
+COMMON_FLAGS += -D__RISCV_FEATURE_DSP=1
+endif
+
+ifneq ($(findstring v,$(ARCH_EXT)),)
+COMMON_FLAGS += -D__RISCV_FEATURE_VECTOR=1
+endif
