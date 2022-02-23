39db96eec3d06c039a95ad9841efa32d7665f8ba
Huaqi Fang <578567190@qq.com>
2022-02-23 16:33:17 +0800

    soc: add __CCM_PRESENT macro in demosoc.h
    __CCM_PRESENT is used to show whether CCM has or not.
    It must be defined to correct value
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/demosoc/Common/Include/demosoc.h
+++ SoC/demosoc/Common/Include/demosoc.h
@@ -227,6 +227,7 @@ typedef enum EXCn {
 #define __PMP_ENTRY_NUM           16                    /*!< Set to 8 or 16, the number of PMP entries */
 #define __ICACHE_PRESENT          0                     /*!< Set to 1 if I-Cache is present */
 #define __DCACHE_PRESENT          0                     /*!< Set to 1 if D-Cache is present */
+#define __CCM_PRESENT             0                     /*!< Set to 1 if Cache Control and Mantainence Unit is present */
 
 #ifndef __INC_INTRINSIC_API
 #define __INC_INTRINSIC_API       0                     /*!< Set to 1 if intrinsic api header files need to be included */
