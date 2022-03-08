52c4b123f91c45292f8e8beeda9d0224b7545e14
Huaqi Fang <578567190@qq.com>
2022-03-08 17:06:57 +0800

    env: add correct qemu path in setup script
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- setup.bat
+++ setup.bat
@@ -11,5 +11,5 @@ if exist %SETUP_CONFIG% call %SETUP_CONFIG%
 echo Setup Nuclei SDK Tool Environment
 echo NUCLEI_TOOL_ROOT=%NUCLEI_TOOL_ROOT%
 
-set PATH=%NUCLEI_TOOL_ROOT%\gcc\bin;%NUCLEI_TOOL_ROOT%\openocd\bin;%NUCLEI_TOOL_ROOT%\build-tools\bin;%NUCLEI_TOOL_ROOT%\qemu\;%PATH%
+set PATH=%NUCLEI_TOOL_ROOT%\gcc\bin;%NUCLEI_TOOL_ROOT%\openocd\bin;%NUCLEI_TOOL_ROOT%\build-tools\bin;%NUCLEI_TOOL_ROOT%\qemu\bin;%PATH%
 @echo on
