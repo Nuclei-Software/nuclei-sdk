2e09b6b09655d6c339f03b256d484184e77932e6
Huaqi Fang <578567190@qq.com>
2022-03-17 18:57:25 +0800

    SoC: porting for libncrt 2.0.0/emrun 4.12.1
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/demosoc/Common/Source/Stubs/libncrt/stubs.c
+++ SoC/demosoc/Common/Source/Stubs/libncrt/stubs.c
@@ -27,11 +27,16 @@ void exit(int fd)
 // __libc_fini_array and __libc_init_array function
 __WEAK void __libc_fini_array(void)
 {
-
 }
+
+// required by libncrt_2.0.0 in RISC-V GNU Toolchain 2022.06
+extern void __SEGGER_RTL_init_heap(void *ptr, size_t size);
+extern char __heap_start[];
+extern char __heap_end[];
 
 __WEAK void __libc_init_array(void)
 {
-
+    size_t heapsz = (size_t)__heap_end - (size_t)__heap_start;
+    __SEGGER_RTL_init_heap((void *)__heap_start, heapsz);
 }
 
