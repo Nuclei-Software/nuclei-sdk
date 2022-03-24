9f7ff47bbe26c290a114451a6918183f3d4b8788
Huaqi Fang <578567190@qq.com>
2022-03-24 17:34:51 +0800

    tools: update barebench runner scripts
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- tools/scripts/misc/barebench/run.sh
+++ tools/scripts/misc/barebench/run.sh
@@ -14,15 +14,36 @@ if [ "x$NSDK_ROOT" == "x" ] ; then
     NSDK_ROOT=$(readlink -f $SCRIPTDIR/../../../..)
 fi
 
+rm -rf $LOGROOT
 if [ ! -d $LOGROOT ] ; then
     mkdir -p $LOGROOT
 fi
 
 NSDK_RUNNER_PY="$NSDK_ROOT/tools/scripts/nsdk_cli/nsdk_runner.py"
 
-RUNNER_CMD="python3 $NSDK_RUNNER_PY --appyaml $SCRIPTDIR/barebench.yaml --logdir $LOGROOT --runon $RUNON --cfgloc $SCRIPTDIR"
+function runbench {
+    local yfn=$1
+    local logdir=$2
+    local mkoptions=${@:3}
 
-echo $RUNNER_CMD
-if [[ $DRYRUN == 0 ]] ; then
-    eval $RUNNER_CMD
-fi
+    local RUNNER_CMD="python3 $NSDK_RUNNER_PY --appyaml $SCRIPTDIR/$yfn.yaml --logdir $LOGROOT/$logdir --runon $RUNON --cfgloc $SCRIPTDIR"
+
+    if [ "x$mkoptions" != "x" ] ; then
+        RUNNER_CMD="${RUNNER_CMD} --make_options \"$mkoptions\""
+    fi
+
+    echo $RUNNER_CMD
+    if [[ $DRYRUN == 0 ]] ; then
+        eval $RUNNER_CMD
+    fi
+}
+
+runbench barebench barebench ""
+
+for dhrymode in ground inline best
+do
+    MKOPTS="DHRY_MODE=$dhrymode"
+    dhrylogdir=dhrystone/$dhrymode
+    runbench dhrystone $dhrylogdir "$MKOPTS"
+done
+
