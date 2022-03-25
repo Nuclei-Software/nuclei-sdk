03d91ba4bbf8d70a9f3127b206fcd28d561b5007
Huaqi Fang <578567190@qq.com>
2022-03-25 17:01:04 +0800

    tools: optimize dobench script using env.sh
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- tools/scripts/misc/dobench/run.sh
+++ tools/scripts/misc/dobench/run.sh
@@ -1,91 +1,28 @@
 #!/bin/env bash
-TOOL_VER=${TOOL_VER:-2022.01}
 SIMU_OPTS=${SIMU_OPTS:-"SIMULATION=1 SIMU=xlspike"}
-NSDK_ROOT=${NSDK_ROOT:-}
-NSDK_VER=${NSDK_VER:-}
-LOGROOT=${LOGROOT:-gen}
-DRYRUN=${DRYRUN:-0}
 RUNTARGET=${RUNTARGET-xlspike}
 
-DEVTOOL_ENV=${DEVTOOL_ENV:-/home/share/devtools/env.sh}
-
 SCRIPTDIR=$(dirname $(readlink -f $BASH_SOURCE))
-if [ "x$NSDK_ROOT" == "x" ] ; then
-    NSDK_ROOT=$(readlink -f $SCRIPTDIR/../../../..)
-fi
+SCRIPTDIR=$(readlink -f $SCRIPTDIR)
+COMMON_ENV=$(readlink -f $SCRIPTDIR/../env.sh)
+DOBENCH_CONFLOC=$SCRIPTDIR
 
-if [ ! -d $LOGROOT ] ; then
-    mkdir -p $LOGROOT
-fi
+source $COMMON_ENV
 
-function describe_repo {
-    local repodir=${1}
-    local repodesc=${2:-repogit.txt}
-    repodir=$(readlink -f $repodir)
-    if [ -d ${repodir}/.git  ] ; then
-        pushd ${repodir}
-        echo "Git Repo $repodir Information:" >> ${repodesc}
-        gitver=$(git describe --tags --always --abbrev=10 --dirty)
-        gitslog=$(git log --oneline -1)
-        echo "git describe version: $gitver" >> ${repodesc}
-        echo "git shortlog: $gitslog" >> ${repodesc}
-        git submodule >> ${repodesc}
-        popd
-    else
-        echo "$repodir not a git repo" >> ${repodesc}
+gen_logdir dobench
+describe_env
+
+function genreport {
+    local logdir=${1:-$LOGDIR}
+    REPORT_CMD="python3 $NSDK_REPORT_PY --logdir $logdir --split"
+    if [ "x$RUNTARGET" != "x" ] ; then
+        REPORT_CMD="$REPORT_CMD --run"
+    fi
+    echo $REPORT_CMD
+    if [[ $DRYRUN == 0 ]] ; then
+        eval $REPORT_CMD
     fi
 }
-
-function describe_build {
-    logfile=$1
-    echo -n "Build Date: " > $logfile
-    date >> $logfile
-    echo "Nuclei GNU Toolchain Version:" >> $logfile
-    riscv-nuclei-elf-gcc -v >> $logfile 2>&1
-}
-
-function describe_sdk {
-    logfile=$1
-    echo -n "Nuclei SDK(npk) " >> $logfile
-    cat $NSDK_ROOT/npk.yml | grep version 2>&1 >> $logfile
-}
-
-function get_sdk_ver {
-    local repodir=${1}
-    repodir=$(readlink -f $repodir)
-    if [ -d ${repodir}/.git  ] ; then
-        pushd ${repodir} > /dev/null
-        gitver=$(git describe --tags --always --abbrev=10 --dirty)
-        popd > /dev/null
-        echo $gitver
-    else
-        cat $NSDK_ROOT/npk.yml | grep version 2>&1
-    fi
-}
-
-if [ "x$NSDK_VER" == "x" ] ; then
-    NSDK_VER=$(get_sdk_ver $NSDK_ROOT)
-fi
-
-echo "Nuclei SDK location: $NSDK_ROOT, SDK Version $NSDK_VER"
-
-LOGDIR=$(readlink -f $LOGROOT)/$NSDK_VER
-NSDK_BENCH_PY="$NSDK_ROOT/tools/scripts/nsdk_cli/nsdk_bench.py"
-NSDK_REPORT_PY="$NSDK_ROOT/tools/scripts/nsdk_cli/nsdk_report.py"
-
-if [ -f $DEVTOOL_ENV ] ; then
-    TOOL_VER=$TOOL_VER source $DEVTOOL_ENV
-else
-    pushd $NSDK_ROOT
-    source setup.sh
-    popd
-fi
-
-mkdir -p $LOGDIR
-
-describe_build $LOGDIR/build.txt
-describe_sdk $LOGDIR/build.txt
-describe_repo $NSDK_ROOT $LOGDIR/build.txt
 
 function runbench {
     local appcfg=$1
@@ -104,56 +41,71 @@ function runbench {
     fi
 }
 
-echo "Run for all benchmark scripts"
-pushd $NSDK_ROOT
-for core in n200 n300 n600 n900 nx600 nx900
-do
-    echo "Build for CORE: $core"
-    appcfg=$SCRIPTDIR/app.json
-    hwcfg=$SCRIPTDIR/bench_${core}.json
-    logdir=$LOGDIR/barebench/$core
-
-    runbench $appcfg $hwcfg $logdir
-done
-
-REPORT_CMD="python3 $NSDK_REPORT_PY --logdir $LOGDIR/barebench --split"
-if [ "x$RUNTARGET" != "x" ] ; then
-    REPORT_CMD="$REPORT_CMD --run"
-fi
-echo $REPORT_CMD
-if [[ $DRYRUN == 0 ]] ; then
-    eval $REPORT_CMD
-fi
-
-echo "Run for dhrystone different modes"
-for core in n200 n300 n600 n900 nx600 nx900
-do
-    for dhrymode in ground inline best
+function runbench_for_allcores {
+    local logdir=${1:-$LOGDIR}
+    pushd $NSDK_ROOT
+    echo "Run benchmark on different CORE"
+    local runlogdir=$logdir/barebench
+    for core in n200 n300 n600 n900 nx600 nx900
     do
-        echo "Build for CORE: $core DHRY_MODE=$dhrymode"
-        appcfg=$SCRIPTDIR/dhry.json
-        hwcfg=$SCRIPTDIR/bench_${core}.json
-        logdir=$LOGDIR/dhrystone/$dhrymode/$core
-        mkopts="DHRY_MODE=$dhrymode"
+        echo "Build for CORE: $core"
+        local appcfg=${DOBENCH_CONFLOC}/app.json
+        local hwcfg=${DOBENCH_CONFLOC}/bench_${core}.json
+        local corelogdir=$runlogdir/$core
 
-        runbench $appcfg $hwcfg $logdir $mkopts
+        runbench $appcfg $hwcfg $corelogdir
     done
-done
+    genreport $runlogdir
+    popd
+}
 
-REPORT_CMD="python3 $NSDK_REPORT_PY --logdir $LOGDIR/dhrystone --split"
-if [ "x$RUNTARGET" != "x" ] ; then
-    REPORT_CMD="$REPORT_CMD --run"
-fi
-echo $REPORT_CMD
-if [[ $DRYRUN == 0 ]] ; then
-    eval $REPORT_CMD
-fi
+function rundhry_for_allcores {
+    local logdir=${1:-$LOGDIR}
+    pushd $NSDK_ROOT
+    echo "Run for dhrystone different modes"
+    local runlogdir=$logdir/dhrystone
+    for core in n200 n300 n600 n900 nx600 nx900
+    do
+        for dhrymode in ground inline best
+        do
+            echo "Build for CORE: $core DHRY_MODE=$dhrymode"
+            local appcfg=${DOBENCH_CONFLOC}/dhry.json
+            local hwcfg=${DOBENCH_CONFLOC}/bench_${core}.json
+            local corelogdir=$runlogdir/$dhrymode/$core
+            local mkopts="DHRY_MODE=$dhrymode"
 
-popd
+            runbench $appcfg $hwcfg $corelogdir $mkopts
+        done
+    done
+    genreport $runlogdir
+    popd
+}
 
-benchzip=barebench_$NSDK_VER.zip
-echo "collect all generated elf and verilog files to $benchzip"
-zip -q -r $benchzip $LOGDIR
-echo "Copy to your local machine using command below"
-echo "scp $(whoami)@$(hostname):$(pwd)/$benchzip ."
+function run_for_allmodes {
+    local logdir=${1:-$LOGDIR}
+    echo "Generate for all run modes"
+    for runmode in "" lm icdlm dcilm cache bus clm
+    do
+        echo "Generate RUNMODE=$runmode"
+        export RUNMODE=$runmode
+        if [ "x$RUNMODE" == "x" ] ; then
+            runmode=default
+        fi
+        runbench_for_allcores $logdir/$runmode
+        rundhry_for_allcores $logdir/$runmode
+        unset RUNMODE
+    done
+}
 
+function run_for_one {
+    echo "Generate only for default run mode"
+    unset RUNMODE
+    local logdir=${1:-$LOGDIR}
+    runbench_for_allcores $logdir/default
+    rundhry_for_allcores $logdir/default
+
+}
+
+run_for_allmodes | tee $LOGDIR/build.log
+
+zip_logdir
