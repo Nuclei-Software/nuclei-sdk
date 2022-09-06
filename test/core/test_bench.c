2537bbfec9d573ee1d5fd255743b6d08c04913ad
Huaqi Fang <578567190@qq.com>
2022-09-06 09:35:38 +0800

    test/core: add more comments about how to use hpm bench api
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- test/core/test_bench.c
+++ test/core/test_bench.c
@@ -19,19 +19,29 @@ CTEST(bench, bench)
     BENCH_ERROR();
     BENCH_STATUS();
 }
-
+// Declare HPMCOUNTER3 and HPMCOUNTER4
 HPM_DECLARE_VAR(3);
 HPM_DECLARE_VAR(4);
 
+// Define HPMEVENT3 and HPMEVENT4
+// HPMEVENT3 value macro: event_sel: 0, event_idx: 8, m/s/uevent_enable: 0xf
+// means Select the instruction commit events, record conditional branch event for all M/S/U mode
 #define HPM_EVENT3      HPM_EVENT(0, 8, 0xf)
+
+// HPMEVENT4 value macro: event_sel: 0, event_idx: 4, m/s/uevent_enable: 0x1
+// means Select the instruction commit events, record integer store instruction (includes SC) event for only U mode
 #define HPM_EVENT4      HPM_EVENT(0, 4, 0x1)
 
 CTEST(bench, hpm)
 {
+    // Init HPM bench, only need to do it once
     HPM_INIT();
+
+    // start to record hpm3 and hpm4
     HPM_START(3, memset, HPM_EVENT3);
     HPM_START(4, memset, HPM_EVENT4);
     memset(test_mem, 0x5a, sizeof(test_mem));
+    // finish record and print hpm value
     HPM_END(3, memset, HPM_EVENT3);
     HPM_END(4, memset, HPM_EVENT4);
 }
