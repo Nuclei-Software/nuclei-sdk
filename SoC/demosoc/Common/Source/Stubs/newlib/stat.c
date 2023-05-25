8f5a6f88b280afab20f42a1f968da6e71ec30db2
Huaqi Fang <578567190@qq.com>
2023-05-25 10:05:15 +0800

    SoC: no longer maintain support for old fashion demosoc, use evalsoc now
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/demosoc/Common/Source/Stubs/newlib/stat.c
+++
@@ -1,9 +0,0 @@
-/* See LICENSE of license details. */
-#include "nuclei_sdk_soc.h"
-#include <sys/stat.h>
-
-__WEAK int _stat(char* file, struct stat* st)
-{
-    st->st_mode = S_IFCHR;
-    return 0;
-}
