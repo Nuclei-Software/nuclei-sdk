72f2632a8b7142b402d7ca7523d8feb969211cc6
Huaqi Fang <578567190@qq.com>
2023-06-07 17:35:12 +0800

    tools: script to rename current release of nmsis dsp/nn library to match gcc13
    old b p v -> zve32f/v zba_zbb_zbc_zbs xxldspn1x
    
    order should be v b p order
    
    example:
    
    - ARCH_EXT=bpv for 32bit changed to ARCH_EXT=_zve32f_zba_zbb_zbc_zbs_xxldspn1x
    - ARCH_EXT=bpv for 64bit changed to ARCH_EXT=v_zba_zbb_zbc_zbs_xxldspn1x
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ tools/scripts/misc/rename_nmsis_lib.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+SCRIPTDIR=$(dirname $(readlink -f $BASH_SOURCE))
+SCRIPTDIR=$(readlink -f $SCRIPTDIR)
+
+NMSIS_LIBDIR=$(readlink -f $SCRIPTDIR/../../../NMSIS/Library)
+
+function rename_nmsis_lib() {
+    local libname=${1:-dsp}
+    for xlen in 32 64
+    do
+        for arch in imac imafc imafdc
+        do
+            for ext in bpv bv bp pv p v b
+            do
+                newext=""
+                vext=_zve32f
+                if [ "$xlen" == "64" ] ; then
+                    vext=v
+                fi
+                case $ext in
+                    bpv)
+                        newext=${vext}_zba_zbb_zbc_zbs_xxldspn1x
+                        ;;
+                    bp)
+                        newext=_zba_zbb_zbc_zbs_xxldspn1x
+                        ;;
+                    bv)
+                        newext=${vext}_zba_zbb_zbc_zbs
+                        ;;
+                    pv)
+                        newext=${vext}_xxldspn1x
+                        ;;
+                    p)
+                        newext=_xxldspn1x
+                        ;;
+                    b)
+                        newext=_zba_zbb_zbc_zbs
+                        ;;
+                    v)
+                        newext=${vext}
+                        ;;
+                esac
+                srclib=libnmsis_${libname}_rv${xlen}${arch}${ext}.a
+                dstlib=libnmsis_${libname}_rv${xlen}${arch}${newext}.a
+                if [ -f $srclib ] && [ "$ext" != "$newext" ] ; then
+                    echo "Rename $srclib -> $dstlib"
+                    mv $srclib $dstlib
+                fi
+            done
+        done
+    done
+}
+
+for libname in dsp nn
+do
+    pushd $NMSIS_LIBDIR/${libname^^}/GCC
+    echo ">>> Renaming library for NMSIS $libname library"
+    rename_nmsis_lib $libname
+    popd
+done
