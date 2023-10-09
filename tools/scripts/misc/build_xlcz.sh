e6131cc9c838d3a36ea7819753ee9bd155345776
Huaqi Fang <578567190@qq.com>
2023-10-09 18:58:03 +0800

    tools: add a script to build xlcz elfs for rtl simulation
    You can use it like this:
    
    > Require gcc13 and qemu 8.0 >= 2023.10.9 version
    
    export NSDK_ROOT=/path/to/nuclei_sdk
    cd ${NSDK_ROOT}/application/baremetal/benchmark/coremark
    ${NSDK_ROOT}/tools/scripts/misc/build_xlcz.sh
    > For whetstone, more timeout time required, it will use 20s not 3s by default
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ tools/scripts/misc/build_xlcz.sh
@@ -0,0 +1,54 @@
+#!/bin/env bash
+
+# how to use
+# cd <Nuclei SDK>/application/baremetal/benchmark/coremark
+# /path/to/build_xlcz.sh [CORE] [BASEEXT] [TOLOC]
+
+# CORE used to run on
+CORE=${1:-n300f}
+# BASE Extension on this CORE
+BASEEXT=${2:-_zba_zbb_zbc_zbs}
+# Copy to location
+TOLOC=${3:-${HOME}/benchelfs/xlcz}
+
+DATE=$(date +%Y%m%d)
+
+CASE=$(basename $(pwd))
+
+# for whetstone need to pass TIMEOUT=20s /path/to/build_xlcz.sh
+if [ "x$CASE" = "xwhetstone" ] ; then
+    TIMEOUT=${TIMEOUT:-20s}
+else
+    TIMEOUT=${TIMEOUT:-3s}
+fi
+
+mkdir -p $TOLOC
+
+echo "INFO: Benchmark $CASE run on $CORE with base extension $BASEEXT, generated elfs will be copy to $TOLOC, TIMEOUT $TIMEOUT"
+
+echo "text data bss dec filename cminscnt xlczinscnt valonqemu valonrtl"
+for ext in "" _xxlczbitop _xxlcz_nobitop _xxlcz _zca_zcb_zcf_zcmp_zcmt _zca_zcb_zcf_zcmp_zcmt_xxlcz ; do
+    archext=${BASEEXT}$ext
+    runarchext=$archext
+    toelf=${TOLOC}/${CASE}_${CORE}${archext}_${DATE}_sim.elf
+    makeopts=""
+    if [[ "$ext" == *"_nobitop"* ]] ; then
+        makeopts="APP_COMMON_FLAGS=\"-DNO_EXTRACTU\""
+        archext=${archext/_nobitop/}
+    fi
+    if [[ "$ext" == *"bitop"* ]] ; then
+        runarchext=${archext/bitop/}
+    fi
+
+    #echo "Build for ${CORE}${archext} -> $toelf"
+    outstr=$(make CORE=$CORE ARCH_EXT=$archext $makeopts SIMULATION=1 SILENT=1 clean all | tail -n1)
+    read -a parts <<< "$outstr"
+    szstr="${parts[@]:0:4}"
+    outstr=$(timeout --foreground ${TIMEOUT} make CORE=$CORE ARCH_EXT=$runarchext SIMULATION=1 SILENT=1 run_qemu 2>&1 | grep "CSV" | tail -n1)
+    read -a parts <<< "$outstr"
+    benchval=${parts[-1]}
+    command cp -f ${CASE}.elf $toelf
+    xlczinscnt=$(riscv64-unknown-elf-objdump -d -M no-aliases $toelf | grep -e "xl\." | wc -l)
+    cminscnt=$(riscv64-unknown-elf-objdump -d -M no-aliases $toelf | grep -e "[c|cm]\." | wc -l)
+    echo "$szstr $toelf $cminscnt $xlczinscnt $benchval"
+done
