330442d664fd2a7551e430fa729d580d28a0671a
Huaqi Fang <578567190@qq.com>
2023-11-17 17:34:34 +0800

    application: optimize dhrystone benchmark options for 900
    ux900fdb ux900_best_config_vcu118_50M_f165df0fe_542e751a6_202311131554.bit
    
    gcc13 3.043 -> 3.127 better than gcc10 3.093
    
    CSV, Benchmark, Iterations, Cycles, DMIPS/MHz
    CSV, Dhrystone, 500000, 91000343, 3.127
    
    If add  -fselective-scheduling, it become 3.076
    
    n900fdb n900_best_config_mmu_vcu118_50M_f165df0fe_542e751a6_202311131517.bit
    
    gcc13 2.736 -> 3.736   worse than gcc10 2.831
    
    CSV, Benchmark, Iterations, Cycles, DMIPS/MHz
    CSV, Dhrystone, 500000, 104000330, 2.736
    
    If add  -fselective-scheduling, it become 2.803
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- application/baremetal/benchmark/dhrystone/toolchain_nuclei_gnu.mk
+++ application/baremetal/benchmark/dhrystone/toolchain_nuclei_gnu.mk
@@ -1,7 +1,7 @@
 ifeq ($(DHRY_MODE),ground)
-BENCH_FLAGS ?= -O3 -flto -fno-inline -funroll-loops -Wno-implicit -mexplicit-relocs -fno-builtin-printf -fno-common -falign-functions=4 -falign-jumps=4 -falign-loops=4
+BENCH_FLAGS ?= -O3 -flto -fno-inline -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8
 else ifeq ($(DHRY_MODE),best)
-BENCH_FLAGS ?= -Ofast -flto -fwhole-program -finline -funroll-loops -Wno-implicit -mexplicit-relocs -fno-builtin-printf -fno-common -falign-functions=4 -falign-jumps=4 -falign-loops=4 -finline-functions
+BENCH_FLAGS ?= -Ofast -flto -fwhole-program -finline -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8 -finline-functions
 else ifeq ($(DHRY_MODE),inline)
-BENCH_FLAGS ?= -O3 -flto -finline -funroll-loops -Wno-implicit -mexplicit-relocs -fno-builtin-printf -fno-common -falign-functions=4 -falign-jumps=4 -falign-loops=4 -finline-functions
+BENCH_FLAGS ?= -O3 -flto -finline -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8 -finline-functions
 endif
--- application/baremetal/benchmark/dhrystone/toolchain_nuclei_llvm.mk
+++ application/baremetal/benchmark/dhrystone/toolchain_nuclei_llvm.mk
@@ -1,7 +1,7 @@
 ifeq ($(DHRY_MODE),ground)
-BENCH_FLAGS ?= -Ofast -flto -fno-inline -funroll-loops -Wno-implicit -fno-builtin-printf -falign-functions=4 -falign-loops=4
+BENCH_FLAGS ?= -Ofast -flto -fno-inline -funroll-loops -falign-functions=4 -falign-loops=4
 else ifeq ($(DHRY_MODE),best)
-BENCH_FLAGS ?= -Ofast -flto -finline -funroll-loops -Wno-implicit -fno-builtin-printf -falign-functions=4 -falign-loops=4 -finline-functions
+BENCH_FLAGS ?= -Ofast -flto -finline -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions
 else ifeq ($(DHRY_MODE),inline)
-BENCH_FLAGS ?= -O3 -flto -finline -funroll-loops -Wno-implicit -fno-builtin-printf -falign-functions=4 -falign-loops=4 -finline-functions
+BENCH_FLAGS ?= -O3 -flto -finline -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions
 endif
