2d01dd109dcf710c4215c3896e043b07415d81d9
Huaqi Fang <578567190@qq.com>
2023-11-20 10:44:16 +0800

    application: add missing -fno-builtin-printf to avoid dhrystone link and run error
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- application/baremetal/benchmark/dhrystone/toolchain_nuclei_gnu.mk
+++ application/baremetal/benchmark/dhrystone/toolchain_nuclei_gnu.mk
@@ -1,7 +1,7 @@
 ifeq ($(DHRY_MODE),ground)
-BENCH_FLAGS ?= -O3 -flto -fno-inline -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8
+BENCH_FLAGS ?= -O3 -flto -fno-inline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8
 else ifeq ($(DHRY_MODE),best)
-BENCH_FLAGS ?= -Ofast -flto -fwhole-program -finline -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8 -finline-functions
+BENCH_FLAGS ?= -Ofast -flto -fwhole-program -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8 -finline-functions
 else ifeq ($(DHRY_MODE),inline)
-BENCH_FLAGS ?= -O3 -flto -finline -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8 -finline-functions
+BENCH_FLAGS ?= -O3 -flto -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-jumps=4 -falign-loops=8 -finline-functions
 endif
--- application/baremetal/benchmark/dhrystone/toolchain_nuclei_llvm.mk
+++ application/baremetal/benchmark/dhrystone/toolchain_nuclei_llvm.mk
@@ -1,7 +1,7 @@
 ifeq ($(DHRY_MODE),ground)
-BENCH_FLAGS ?= -Ofast -flto -fno-inline -funroll-loops -falign-functions=4 -falign-loops=4
+BENCH_FLAGS ?= -Ofast -flto -fno-inline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4
 else ifeq ($(DHRY_MODE),best)
-BENCH_FLAGS ?= -Ofast -flto -finline -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions
+BENCH_FLAGS ?= -Ofast -flto -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions
 else ifeq ($(DHRY_MODE),inline)
-BENCH_FLAGS ?= -O3 -flto -finline -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions
+BENCH_FLAGS ?= -O3 -flto -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions
 endif
