e868cff64533409fd2589e35bd7ab9c2d79eff2a
Huaqi Fang <578567190@qq.com>
2024-01-09 17:17:15 +0800

    OS: Add ThreadX source code
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ OS/ThreadX/build.mk
@@ -0,0 +1,5 @@
+C_SRCDIRS += $(NUCLEI_SDK_RTOS)/common/src $(NUCLEI_SDK_RTOS)/ports/nuclei
+
+ASM_SRCDIRS += $(NUCLEI_SDK_RTOS)/ports/nuclei/gnu
+
+INCDIRS += $(NUCLEI_SDK_RTOS)/common/inc $(NUCLEI_SDK_RTOS)/ports/nuclei
---
+++ OS/ThreadX/ports/nuclei/port.c
@@ -0,0 +1,17 @@
+#include "tx_api.h"
+
+
+VOID        _tx_initialize_low_level(VOID)
+{
+
+}
+
+VOID _tx_thread_schedule(VOID)
+{
+
+}
+
+VOID        _tx_thread_stack_build(TX_THREAD *thread_ptr, VOID (*function_ptr)(VOID))
+{
+
+}
