621175b2c9dcd0c4b94c195be3a3f960ea9333fd
Huaqi Fang <578567190@qq.com>
2024-06-17 10:22:30 +0800

    SoC/evalsoc: add more comments in cpufeature.h
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/evalsoc/Common/Include/cpufeature.h
+++ SoC/evalsoc/Common/Include/cpufeature.h
@@ -7,8 +7,11 @@ extern "C" {
 
 
 // NOTE: This header file is used to tell us the Nuclei CPU Feature
-// Currently for the opensource evalsoc's cpu feature, we manually create this header file
+// Currently for the evalsoc's cpu feature, we manually create this header file
 // but for our customer, you can use nuclei_gen tool to generate it, and just replace it
+// If you want to create it on your own, please modify these files:
+// cpufeature.h, cpufeature.mk, openocd_evalsoc.cfg and evalsoc.memory
+// and the iar project linker script files *.icf
 // TODO: You can replace this file using the generated one by nuclei_gen tool
 
 #define CFG_CPU_SERIES              0xFFFFFF
@@ -26,8 +29,6 @@ extern "C" {
 // PMONITOR is the mcycle and minsret counter, if not defined, it means not present
 #define CFG_HAS_PMONITOR
 
-// CCM default still set to 0, you can uncomment it when it really exist
-// #define CFG_HAS_IOCC
 #define CFG_ILM_BASE_ADDR           (0x80000000UL)
 #define CFG_DLM_BASE_ADDR           (0x90000000UL)
 
