49e522659f9f7ea3884468b44278835e6afa9351
Huaqi Fang <578567190@qq.com>
2024-07-17 16:39:15 +0800

    SoC/evalsoc: mention about -mstrict-align for 200 series without unalign memory access
    User should add -mstrict-align option during compiling, see SoC/evalsoc/cpufeature.mk
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/evalsoc/cpufeature.mk
+++ SoC/evalsoc/cpufeature.mk
@@ -1,3 +1,10 @@
 # TODO: You can replace this file using the generated one by nuclei_gen tool
 CORE ?= n300fd
 ARCH_EXT ?=
+
+# TODO: For CPU without unaligned load/store feature
+# eg. If you are using Nuclei 200 Series CPU, and your cpu are configured without
+# unaligned load/store access support, you should uncomment below lines to make sure
+# not generate unaligned memory access
+# see -mstrict-align in https://gcc.gnu.org/onlinedocs/gcc/RISC-V-Options.html
+#COMMON_FLAGS += -mstrict-align
