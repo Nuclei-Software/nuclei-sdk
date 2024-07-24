f2c68958db5ff6d7d1dfc05ae60fa2c2e179bfc2
Huaqi Fang <578567190@qq.com>
2024-07-24 15:50:22 +0800

    SoC/evalsoc: change default SYSTEM_CLOCK from 80M to 16M
    16M is the most used frequency for n200/n300 bitstream
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/evalsoc/Common/Source/system_evalsoc.c
+++ SoC/evalsoc/Common/Source/system_evalsoc.c
@@ -36,7 +36,7 @@
 /* ToDo: add here your necessary defines for device initialization
          following is an example for different system frequencies */
 #ifndef SYSTEM_CLOCK
-#define SYSTEM_CLOCK    (80000000UL)
+#define SYSTEM_CLOCK    (16000000UL)
 #endif
 
 /**
