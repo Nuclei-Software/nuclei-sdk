9ca5d7406006513fc222629dc9966508fed6ab5e
Huaqi Fang <578567190@qq.com>
2024-07-25 16:28:02 +0800

    Build: Add QEMU_MC_EXTOPT and QEMU_CPU_EXTOPT make variable for qemu of evalsoc
    QEMU_MC_EXTOPT is for extra qemu machine options
    QEMU_CPU_EXTOPT is for extra qemu cpu options
    
    eg.
    
    cd application/baremetal/demo_dsp
    
    $ make CORE=nx900fd ARCH_EXT=v QEMU_CPU_EXTOPT=vlen=1024 QEMU_MC_EXTOPT="debug
    =1" run_qemu
    Assembling :  ../../../SoC/evalsoc/Common/Source/GCC/intexc_evalsoc.S
    Assembling :  ../../../SoC/evalsoc/Common/Source/GCC/intexc_evalsoc_s.S
    Assembling :  ../../../SoC/evalsoc/Common/Source/GCC/startup_evalsoc.S
    Compiling  :  ../../../SoC/evalsoc/Common/Source/Drivers/evalsoc_uart.c
    Compiling  :  ../../../SoC/evalsoc/Common/Source/Stubs/newlib/stubs.c
    Compiling  :  ../../../SoC/evalsoc/Common/Source/evalsoc_common.c
    Compiling  :  ../../../SoC/evalsoc/Common/Source/system_evalsoc.c
    Compiling  :  main.c
    Compiling  :  math_helper.c
    Compiling  :  ref_conv.c
    Compiling  :  ref_helper.c
    Linking    :  demo_dsp.elf
       text    data     bss     dec     hex filename
      27600    5952   11552   45104    b030 demo_dsp.elf
    Run program demo_dsp.elf on qemu-system-riscv64
    qemu-system-riscv64 -M nuclei_evalsoc,download=ilm,debug=1 -cpu nuclei-nx900fd,ext=v,vlen=1024 -smp 1 -icount shift=0 -nodefaults -nographic -serial stdio -kernel demo_dsp.elf
    vector version is not specified, use the default value v1.0
    download mode is ilm
    ddr     : base:0x80000000, size:0x4000000, startup_addr:0xa0000000
    ilm     : base:0x80000000, size:0x800000, startup_addr:0x80000000
    dlm     : base:0x90000000, size:0x800000, startup_addr:0x90000000
    sram    : base:0xa0000000, size:0x20000000, startup_addr:0xa0000000
    norflash: base:0x20000000, size:0x2000000, startup_addr:0x20000000
    flash   : base:0x20000000, size:0x2000000, startup_addr:0x20000000
    uart0   : base:0x10013000, irq:33
    uart1   : base:0x10023000, irq:34
    qspi0   : base:0x10014000, irq:35
    qspi1   : base:0x10024000, irq:36
    qspi2   : base:0x10034000, irq:37
    iregion : 0x18000000
    irqmax  : 40
    firmware startup addr:0x80000000
    Nuclei SDK Build Time: Jul 25 2024, 16:29:51
    Download Mode: ILM
    CPU Frequency 1000001699 Hz
    CPU HartID: 0
    
    Nuclei RISC-V NMSIS-DSP Library Demonstration
    Using Nuclei RISC-V accelerated and optimized NMSIS-DSP library!
    Warning: Make sure Nuclei RISC-V Vector is present in your CPU core!
             Otherwise this example will trap to cpu core exception!
    
    Benchmark initialized
    CSV, riscv_conv_q31, 16721
    CSV, ref_conv_q31, 6402312
    SUCCESS, riscv_conv_q31
    CSV, riscv_conv_q15, 4188
    CSV, ref_conv_q15, 739487
    SUCCESS, riscv_conv_q15
    CSV, riscv_conv_q7, 288
    CSV, ref_conv_q7, 2219
    SUCCESS, riscv_conv_q7
    CSV, riscv_conv_fast_q15, 3287
    CSV, ref_conv_fast_q15, 724510
    SUCCESS, riscv_conv_fast_q15
    CSV, riscv_conv_fast_q31, 553775
    CSV, ref_conv_fast_q31, 6582312
    SUCCESS, riscv_conv_fast_q31
    CSV, riscv_conv_opt_q15, 4193
    CSV, ref_conv_opt_q15, 739487
    SUCCESS, riscv_conv_opt_q15
    CSV, riscv_conv_opt_q7, 293
    CSV, ref_conv_opt_q7, 2219
    SUCCESS, riscv_conv_opt_q7
    CSV, riscv_conv_fast_opt_q15, 3301
    CSV, ref_conv_fast_opt_q15, 729493
    SUCCESS, riscv_conv_fast_opt_q15
    all test are passed. Well done!
    
    NMSIS_TEST_PASS
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/evalsoc/build.mk
+++ SoC/evalsoc/build.mk
@@ -184,13 +184,19 @@ RISCV_ABI ?= $(word 2, $(CORE_ARCH_ABI))
 RISCV_TUNE ?= $(word 3, $(CORE_ARCH_ABI))
 
 # Handle QEMU Emulation
-QEMU_MACHINE ?= nuclei_evalsoc,download=$(DOWNLOAD)
+# - QEMU_MC_EXTOPT is used to pass extra options to qemu -M machine options for evalsoc,
+#   please dont pass any extra , to this make variable, you can pass such
+#   as QEMU_MC_EXTOPT=debug=1 but not pass QEMU_MC_EXTOPT=,debug=1
+# - QEMU_CPU_EXTOPT is used to pass extra options to qemu -cpu cpu options for evalsoc,
+#   please dont pass any extra , to this make variable, you can pass such
+#   as QEMU_CPU_EXTOPT=vlen=512 but not pass QEMU_CPU_EXTOPT=,vlen=512
+QEMU_MACHINE ?= nuclei_evalsoc,download=$(DOWNLOAD),$(QEMU_MC_EXTOPT)
 # You can pass customized soc configuration here
 ifneq ($(QEMU_SOCCFG),)
 QEMU_MACHINE := $(QEMU_MACHINE),soc-cfg=$(NUCLEI_SDK_SOC)/$(QEMU_SOCCFG)
 endif
 QEMU_ARCHEXT ?= ${ARCH_EXT}
-QEMU_CPU ?= nuclei-$(CORE),ext=$(QEMU_ARCHEXT)
+QEMU_CPU ?= nuclei-$(CORE),ext=$(QEMU_ARCHEXT),$(QEMU_CPU_EXTOPT)
 ifneq ($(SEMIHOST),)
 QEMU_OPT += -semihosting
 endif
