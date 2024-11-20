314faaa445860381bf1e102f4a78b277bd20c306
dongyongtao <dongyongtao@nucleisys.com>
2024-11-20 15:58:21 +0800

    NMSIS/Core: add core_feature_pma.h
    M/SPMA_SetRegion M/SPMA_GetRegion implemented
    
    Signed-off-by: dongyongtao <dongyongtao@nucleisys.com>

--- NMSIS/Core/Include/nmsis_core.h
+++ NMSIS/Core/Include/nmsis_core.h
@@ -92,8 +92,9 @@
 /* Include core cache feature header file */
 #include "core_feature_cache.h"
 /* Include core cidu feature header file */
- #include "core_feature_cidu.h"
-
+#include "core_feature_cidu.h"
+/* Include core pma feature header file */
+#include "core_feature_pma.h"
 /* Include compatiable functions header file */
 #include "core_compatiable.h"
 
