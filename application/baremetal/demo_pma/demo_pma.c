df47042eb48790cf6c2f286f3c399ee59c2eea8d
dongyongtao <dongyongtao@nucleisys.com>
2024-11-20 17:56:44 +0800

    application: add demo_pma
    Signed-off-by: dongyongtao <dongyongtao@nucleisys.com>

---
+++ application/baremetal/demo_pma/demo_pma.c
@@ -0,0 +1,86 @@
+// See LICENSE for license details.
+#include <stdio.h>
+#include <stdlib.h>
+#include "nuclei_sdk_soc.h"
+
+#include "nmsis_bench.h"
+
+#if !defined(__CCM_PRESENT) || (__CCM_PRESENT != 1)
+/* __CCM_PRESENT should be defined in <Device>.h */
+#error "This example require CPU CCM feature!"
+#endif
+
+#if !defined(__DCACHE_PRESENT) || (__DCACHE_PRESENT != 1)
+/* __DCACHE_PRESENT should be defined in <Device>.h */
+#error "This example require CPU DCACHE feature!"
+#endif
+
+#define ROW_SIZE         512
+#define COL_SIZE         64
+// 32KB
+#define REGION_SIZE                     (512 * 64)
+
+// 4K aligned
+uint8_t array_test[ROW_SIZE][COL_SIZE] __attribute__((aligned(0x1000))) = {0};
+
+void array_update_by_row(void)
+{
+    int32_t i, j = 0;
+
+    for (i = 0; i < ROW_SIZE; i++)
+        for (j = 0; j < COL_SIZE; j++) {
+            array_test[i][j] = 0xab;
+        }
+}
+
+BENCH_DECLARE_VAR();
+
+int main(void)
+{
+
+    pma_config pma_cfg_r;
+    pma_config pma_cfg = {
+        // Take care to set the region type and address range, which maybe causing function or performance issue!
+        .region_type = PMA_REGION_TYPE_DEV,
+        .region_base = (unsigned long)&array_test,
+        .region_size = REGION_SIZE,
+        .region_enable = PMA_REGION_ENA
+    };
+
+    if (!DCachePresent()) {
+        printf("DCache not present in CPU!\n");
+        return -1;
+    }
+    BENCH_INIT();
+
+    // To ensure data consistency
+    DisableDCache();
+    MFlushInvalDCache();
+
+    printf("\nSet to NonCacheable region\n\r");
+    pma_cfg.region_type = PMA_REGION_TYPE_NC;
+    PMA_SetRegion(0, &pma_cfg);
+    PMA_GetRegion(0, &pma_cfg_r);
+    printf("Region type: 0x%x,region base addr: 0x%lx, region size: 0x%lx, region status: %d\n\r",\
+        pma_cfg_r.region_type, pma_cfg_r.region_base, pma_cfg_r.region_size, pma_cfg_r.region_enable);
+
+    BENCH_START(NonCacheable);
+    array_update_by_row();
+    BENCH_END(NonCacheable);
+
+    EnableDCache();
+
+    printf("\nSet to Cacheable region\n\r");
+    pma_cfg.region_type = PMA_REGION_TYPE_CA;
+    PMA_SetRegion(0, &pma_cfg);
+    PMA_GetRegion(0, &pma_cfg_r);
+    printf("Region type: 0x%x,region base addr: 0x%lx, region size: 0x%lx, region status: %d\n\r",\
+        pma_cfg_r.region_type, pma_cfg_r.region_base, pma_cfg_r.region_size, pma_cfg_r.region_enable);
+
+    BENCH_START(Cacheable);
+    array_update_by_row();
+    BENCH_END(Cacheable);
+
+    return 0;
+}
+
