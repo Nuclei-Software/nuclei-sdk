1ef2600f664dbba6e90459e454ebdf1958b5121c
dongyongtao <dongyongtao@nucleisys.com>
2024-12-26 21:37:56 +0800

    application/demo_pma: enable cache and hot the cache before benchmark, to demostrate the PMA feature and show dcache miss
    Signed-off-by: dongyongtao <dongyongtao@nucleisys.com>

--- application/baremetal/demo_pma/demo_pma.c
+++ application/baremetal/demo_pma/demo_pma.c
@@ -1,6 +1,7 @@
 // See LICENSE for license details.
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "nuclei_sdk_soc.h"
 
 #include "nmsis_bench.h"
@@ -15,27 +16,34 @@
 #error "This example require CPU DCACHE feature!"
 #endif
 
+
 #if !defined(__PMA_PRESENT) || (__PMA_PRESENT != 1)
 /* __PMA_PRESENT should be defined in <Device>.h */
 #error "This example require CPU PMA feature!"
 #endif
 
-#define ROW_SIZE         512
+//#define BIG_ROW_SIZE
+#ifndef BIG_ROW_SIZE
+#define ROW_SIZE         10
+#else
+#define ROW_SIZE         1024
+#endif
 #define COL_SIZE         64
-// 32KB
-#define REGION_SIZE                     (512 * 64)
+// 64KB
+#define REGION_SIZE                     (1024 * 64)
 
 // 4K aligned
 uint8_t array_test[ROW_SIZE][COL_SIZE] __attribute__((aligned(0x1000))) = {0};
 
+// Declare HPMCOUNTER4
+HPM_DECLARE_VAR(4);
+// Means select the Dcache miss events, record Dcache miss event for all M/S/U mode
+#define HPM_EVENT4      HPM_EVENT(EVENT_SEL_MEMORY_ACCESS, EVENT_MEMORY_ACCESS_DCACHE_MISS, MSU_EVENT_ENABLE)
+
+
 void array_update_by_row(void)
 {
-    int32_t i, j = 0;
-
-    for (i = 0; i < ROW_SIZE; i++)
-        for (j = 0; j < COL_SIZE; j++) {
-            array_test[i][j] = 0xab;
-        }
+    memset(array_test, 0xab, sizeof(array_test));
 }
 
 BENCH_DECLARE_VAR();
@@ -43,6 +51,7 @@ BENCH_DECLARE_VAR();
 int main(void)
 {
 #if defined(__CCM_PRESENT) && (__CCM_PRESENT == 1)
+    CacheInfo_Type cacheinfo_type;
     pma_config pma_cfg_r;
     pma_config pma_cfg = {
         // Take care to set the region type and address range, which maybe causing function or performance issue!
@@ -51,15 +60,18 @@ int main(void)
         .region_size = REGION_SIZE,
         .region_enable = PMA_REGION_ENA
     };
-
+    BENCH_INIT();
     if (!DCachePresent()) {
         printf("DCache not present in CPU!\n");
         return -1;
     }
-    BENCH_INIT();
+    GetDCacheInfo(&cacheinfo_type);
+    printf("DCache Linesize is %d bytes, ways is %d, setperway is %d, total size is %d bytes\n\n", cacheinfo_type.linesize, \
+            cacheinfo_type.ways, cacheinfo_type.setperway,cacheinfo_type.size);
+    printf("array_test size: %d * %d bytes, addr: 0x%x\r\n", ROW_SIZE, COL_SIZE, (unsigned long)&array_test);
 
-    // To ensure data consistency
-    DisableDCache();
+    EnableDCache(); // enable dcache here on purpose before setting NonCacheable, which actually takes no effect to array update
+    // To ensure cached data consistency, because it's NonCacheable now
     MFlushInvalDCache();
 
     printf("\nSet to NonCacheable region\n\r");
@@ -68,12 +80,14 @@ int main(void)
     PMA_GetRegion(0, &pma_cfg_r);
     printf("Region type: 0x%x,region base addr: 0x%lx, region size: 0x%lx, region status: %d\n\r",\
         pma_cfg_r.region_type, pma_cfg_r.region_base, pma_cfg_r.region_size, pma_cfg_r.region_enable);
+    // hot the cache here takes no effect if it's NonCacheable
+    array_update_by_row();
 
+    HPM_START(4, array_update_by_row_dcache_miss_noncacheable, HPM_EVENT4);
     BENCH_START(NonCacheable);
     array_update_by_row();
     BENCH_END(NonCacheable);
-
-    EnableDCache();
+    HPM_END(4, array_update_by_row_dcache_miss_noncacheable, HPM_EVENT4);
 
     printf("\nSet to Cacheable region\n\r");
     pma_cfg.region_type = PMA_REGION_TYPE_CA;
@@ -81,13 +95,18 @@ int main(void)
     PMA_GetRegion(0, &pma_cfg_r);
     printf("Region type: 0x%x,region base addr: 0x%lx, region size: 0x%lx, region status: %d\n\r",\
         pma_cfg_r.region_type, pma_cfg_r.region_base, pma_cfg_r.region_size, pma_cfg_r.region_enable);
+    // hot the cache
+    array_update_by_row();
 
+    HPM_START(4, array_update_by_row_dcache_miss_cacheable, HPM_EVENT4);
     BENCH_START(Cacheable);
     array_update_by_row();
     BENCH_END(Cacheable);
+    HPM_END(4, array_update_by_row_dcache_miss_cacheable, HPM_EVENT4);
 #else
     printf("[ERROR]__CCM_PRESENT must be defined as 1 in <Device>.h!\r\n");
 #endif
     return 0;
 }
+
 
