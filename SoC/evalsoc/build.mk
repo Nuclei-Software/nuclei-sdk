d5e14021406ba0cbc63c8b5894a8316e7bc54784
Huaqi Fang <578567190@qq.com>
2025-03-10 15:10:56 +0800

    Build: add run_xlmodel support since nuclei cpu model is ready
    You can use command below to test it, eg.
    
    make CORE=n900fd ARCH_EXT=_xxldsp run_xlmodel
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- SoC/evalsoc/build.mk
+++ SoC/evalsoc/build.mk
@@ -209,6 +209,16 @@ ifneq ($(SEMIHOST),)
 QEMU_OPT += -semihosting
 endif
 
+# Handle XLModel Emulation
+XLMODEL_OPT += -M nuclei_evalsoc
+XLMODEL_OPT += --cpu=$(CORE) --download=$(DOWNLOAD)
+ifneq ($(ARCH_EXT),)
+XLMODEL_OPT += --ext=$(ARCH_EXT)
+endif
+ifneq ($(CPU_CNT),)
+XLMODEL_OPT += --smp=$(CPU_CNT)
+endif
+
 ifneq ($(CODESIZE),)
 COMMON_FLAGS += -DCODESIZE=$(CODESIZE)
 endif
