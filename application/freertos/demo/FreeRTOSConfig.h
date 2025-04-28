b4f86c9720d888f4c3f3cb3abc08733d4970e142
Huaqi Fang <578567190@qq.com>
2025-04-28 19:41:24 +0800

    application: Fix malloc failed for freertos demo for rv64
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- application/freertos/demo/FreeRTOSConfig.h
+++ application/freertos/demo/FreeRTOSConfig.h
@@ -103,7 +103,7 @@ your application. */
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   10*1024
+#define configTOTAL_HEAP_SIZE                   15*1024
 #define configAPPLICATION_ALLOCATED_HEAP        0
 
 /* Hook function related definitions. */
