875775b64a25d2826fd612fd7c7fee29ce69f301
Huaqi Fang <578567190@qq.com>
2025-05-12 13:19:38 +0800

    tools: update hpm_parse.py to support pmu v2

--- tools/scripts/nsdk_cli/hpm_parse.py
+++ tools/scripts/nsdk_cli/hpm_parse.py
@@ -11,14 +11,24 @@ HPM_EVENTS= [
          "Integer store instruction (includes SC)", "Atomic memory operation (do not include LR and SC)",
          "System instruction", "Integer computational instruction(excluding multiplication/division/remainder)",
          "Conditional branch", "Taken conditional branch", "JAL instruction", "JALR instruction", "Return instruction",
-         "Control transfer instruction (CBR+JAL+JALR)", "-", "Integer multiplication instruction",
+         "Control transfer instruction (CBR+JAL+JALR)", "Fence instruction(Not include fence.i)", "Integer multiplication instruction",
          "Integer division/remainder instruction", "Floating-point load instruction", "Floating-point store instruction",
          "Floating-point addition/subtraction", "Floating-point multiplication",
          "Floating-point fused multiply-add (FMADD, FMSUB, FNMSUB, FNMADD)", "Floating-point division or square-root",
          "Other floating-point instruction", "Conditional branch prediction fail", "JALR prediction fail",
          "POP predication fail", "FENCEI instruction", "SFENCE instruction", "ECALL instruction",
          "EXCEPTION instruction", "INTERRUPT instruction"],
-        ["Reserved", "Icache miss", "Dcache miss", "ITLB miss", "DTLB miss", "Main TLB miss"]
+        ["Reserved", "Icache miss", "Dcache miss", "ITLB miss", "DTLB miss", "Main TLB miss",
+         "Reserved", "Reserved", "L2-Cache access count", "L2-Cache miss count",
+         "Memory bus request count", "IFU stall cycle count", "EXU stall cycle count", "Timer count"],
+        ["Reserved", "Reserved", "Branch instruction commit count", "Branch predict fail count"],
+        ["Dcache read count", "Dcache read miss count", "Dcache write count", "Dcache write miss count",
+         "Dcache prefetch count", "Dcache prefetch miss count", "Icache read count", "Reserved",
+         "Icache prefetch count", "Icache prefetch miss count", "L2-Cache read hit count",
+         "L2-Cache read miss count", "L2-Cache write hit count", "L2-Cache write miss count",
+         "L2-Cache prefetch hit count", "L2-Cache prefetch miss count", "DTLB read count",
+         "DTLB read miss count", "DTLB write count", "DTLB write miss count", "ITLB read count",
+         "Reserved", "BTB read count", "BTB read miss count", "BTB write count", "BTB write miss count"]
 ]
 
 HPM_MEVENT_ENABLE = 0x8
