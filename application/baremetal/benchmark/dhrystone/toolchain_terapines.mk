6c4500ddf538ed556344f169d7f4fa756ea54eee
Huaqi Fang <578567190@qq.com>
2025-05-22 13:08:50 +0800

    application: optimize zcc options for n100
    zcc v4.0.0 will bring better performance and code size reduction for nuclei cpu
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- application/baremetal/benchmark/dhrystone/toolchain_terapines.mk
+++ application/baremetal/benchmark/dhrystone/toolchain_terapines.mk
@@ -1,10 +1,8 @@
 ifeq ($(DHRY_MODE),ground)
-BENCH_FLAGS ?= -Ofast -flto -fno-inline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -flate-loop-unroll -malign-branch
-LDFLAGS += -Wl,-mllvm,--align-all-nofallthru-blocks=2
+BENCH_FLAGS ?= -O3 -ffast-math -flto -fno-inline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -flate-loop-unroll -malign-branch
 else ifeq ($(DHRY_MODE),best)
-BENCH_FLAGS ?= -Ofast -flto -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions -flate-loop-unroll -malign-branch
-LDFLAGS += -Wl,-mllvm,--align-all-nofallthru-blocks=2
+BENCH_FLAGS ?= -O3 -ffast-math -flto -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions -flate-loop-unroll -malign-branch
 else ifeq ($(DHRY_MODE),inline)
 BENCH_FLAGS ?= -O3 -flto -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions -flate-loop-unroll -malign-branch
-LDFLAGS += -Wl,-mllvm,--align-all-nofallthru-blocks=2
 endif
+LDFLAGS += -Wl,-mllvm,--align-all-nofallthru-blocks=2 -fuse-size-lib
