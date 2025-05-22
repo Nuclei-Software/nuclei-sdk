35af7fee29f1cd454c830843a5d9b098016a6437
yinghao <73093416+andreww0210@users.noreply.github.com>
2025-05-22 11:16:00 +0800

    application: update zcc 4.0.0 options for dhrystone and coremark (#62)
    * New options for zcc4.0.0
    
    * Add `-fuse-size-lib` for dhrystone and update npk.yml
    
    * `-fuse-size-lib` will decrease the code size

--- application/baremetal/benchmark/dhrystone/toolchain_terapines.mk
+++ application/baremetal/benchmark/dhrystone/toolchain_terapines.mk
@@ -1,5 +1,5 @@
 ifeq ($(DHRY_MODE),ground)
-BENCH_FLAGS ?= -O3 -ffast-math -flto -fno-inline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -flate-loop-unroll -malign-branch
+BENCH_FLAGS ?= -O3 -ffast-math -flto -fno-inline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -flate-loop-unroll -malign-branch -fuse-size-lib
 LDFLAGS += -Wl,-mllvm,--align-all-nofallthru-blocks=2
 else ifeq ($(DHRY_MODE),best)
 BENCH_FLAGS ?= -O3 -ffast-math -flto -finline -fno-builtin-printf -funroll-loops -falign-functions=4 -falign-loops=4 -finline-functions -flate-loop-unroll -malign-branch
