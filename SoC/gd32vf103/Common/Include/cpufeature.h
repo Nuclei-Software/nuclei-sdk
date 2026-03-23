3d606ed4d0ea470d3a2baf1b61388a39615d2c46
Huaqi Fang <578567190@qq.com>
2026-03-23 18:04:46 +0800

    SoC: add missing cpufeature.h files for gd32vf103 and gd32vw55x
    these cpufeature.h files are not suitable for these SoC, but this
    cpufeature.h file is required by RTOS portable code
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

---
+++ SoC/gd32vf103/Common/Include/cpufeature.h
@@ -0,0 +1,14 @@
+#ifndef __CPUFEATURE_H__
+#define __CPUFEATURE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// NOTE: This file is not suitable for gd32vf103, but required by RTOS porting code
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ SoC/gd32vw55x/Common/Include/cpufeature.h
@@ -0,0 +1,14 @@
+#ifndef __CPUFEATURE_H__
+#define __CPUFEATURE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// NOTE: This file is not suitable for gd32vw55x, but required by RTOS porting code
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
